228247f02ca74946e0ad618204db15f55c7a1ab4
Dan <dan.pelgrift@gmail.com>
2022-12-19 23:48:27 -0500

    Converted WGS84 vals to consts

--- include/wgs84.h
+++ include/wgs84.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "Arduino.h"
 
 /*
@@ -8,25 +9,25 @@
 
 
 // WGS 84 Defining Parameters
-float a           = 6378137.0;           // Semi - major Axis[m]
-float a_sqrd      = pow(a, 2);           // Semi - major Axis[m] squared
-float f           = 1.0 / 298.257223563; // Flattening
-float omega_E     = 7292115.0e-11;       // Angular velocity of the Earth[rad / s]
-float omega_E_GPS = 7292115.1467e-11;    // Angular velocity of the Earth[rad / s]
+const float a           = 6378137.0;           // Semi - major Axis[m]
+const float a_sqrd      = pow(a, 2);           // Semi - major Axis[m] squared
+const float f           = 1.0 / 298.257223563; // Flattening
+const float omega_E     = 7292115.0e-11;       // Angular velocity of the Earth[rad / s]
+const float omega_E_GPS = 7292115.1467e-11;    // Angular velocity of the Earth[rad / s]
                                          // According to ICD - GPS - 200
 
-float GM = 3.986004418e14; // Earth's Gravitational Constant [m^3/s^2]
+const float GM = 3.986004418e14; // Earth's Gravitational Constant [m^3/s^2]
                            // (mass of earth's atmosphere included)
 
-float GM_GPS = 3.9860050e14; // The WGS 84 GM value recommended for GPS receiver usage
+const float GM_GPS = 3.9860050e14; // The WGS 84 GM value recommended for GPS receiver usage
                              // by the GPS interface control document(ICD - GPS - 200)
                              // differs from the current refined WGS 84 GM value.
 
 // WGS 84 Ellipsoid Derived Geometric Constants
-float b              = 6356752.3142;       // Semi - minor axis[m]
-float b_sqrd         = pow(b, 2);          // Semi - minor axis[m] squared
-float ecc            = 8.1819190842622e-2; // First eccentricity
-float ecc_sqrd       = pow(ecc, 2);        // First eccentricity squared
-float ecc_prime      = 8.2094437949696e-2; // Second eccentricity
-float ecc_prime_sqrd = pow(ecc_prime, 2);  // Second eccentricity squared
-float r              = (2*a + b) / 3;      // Arithmetic mean radius [m]
+const float b              = 6356752.3142;       // Semi - minor axis[m]
+const float b_sqrd         = pow(b, 2);          // Semi - minor axis[m] squared
+const float ecc            = 8.1819190842622e-2; // First eccentricity
+const float ecc_sqrd       = pow(ecc, 2);        // First eccentricity squared
+const float ecc_prime      = 8.2094437949696e-2; // Second eccentricity
+const float ecc_prime_sqrd = pow(ecc_prime, 2);  // Second eccentricity squared
+const float r              = (2*a + b) / 3;      // Arithmetic mean radius [m]
